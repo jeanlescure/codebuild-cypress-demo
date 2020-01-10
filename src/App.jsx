a88d9821db285f41f0f28288edc99ba5eb4bb87d
Jean M. Lescure <jeanmlescure@gmail.com>
2020-01-10 17:57:11 -0600

    [test] implement button changes h1 message

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { hot } from 'react-hot-loader';
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-const message = 'Welcome to codebuild-cypress-demo';
-const App = () => (
-  <div className="App">
-    <h1>{message}</h1>
-  </div>
-);
+const App = () => {
+  const [message, setMessage] = useState('Welcome to codebuild-cypress-demo');
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+      <button
+        type="button"
+        onClick={() => {
+          setMessage('Button clicked!');
+        }}
+      >
+        hello world
+      </button>
+    </div>
+  );
+};
 
 export default hot(module)(App);
